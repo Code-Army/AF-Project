67425bcd8cf842557f5847fad031b1b641198fc3
madushani <madushanidissanayake1998@gmail.com>
2020-05-18 11:39:17 +0530

    Update product

--- Server/models/manager/product.model.js
+++ Server/models/manager/product.model.js
@@ -12,11 +12,10 @@ const productSchema = new Schema({
         price: {type:String,default: ""},
         category: {type:String,default:""},
         cid: {type:String,default:""},
+        sid: {type:String,default:""},
         subcategory: {type:String,default:""},
         oprice: {type:String,default:""},
         url1 : {type:String, default:""},
-        url2 : {type:String, default:"https://www.google.com/imgres?imgurl=https%3A%2F%2F4.imimg.com%2Fdata4%2FBS%2FKY%2FMY-440847%2Fbaby-soap-250x250.jpg&imgrefurl=https%3A%2F%2Fwww.indiamart.com%2Fproddetail%2Fbath-soaps-13351893388.html&tbnid=UizqKip6xf1WgM&vet=12ahUKEwiXqaeO3rfpAhXdkuYKHRCvAWkQMygKegUIARC2Ag..i&docid=9J4cmP7jyovdbM&w=250&h=250&q=baby%20soaps&safe=active&ved=2ahUKEwiXqaeO3rfpAhXdkuYKHRCvAWkQMygKegUIARC2Ag"},
-        url3 : {type:String, default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.in%2FJohnsons-20005893-Baby-Soap-100g%2Fdp%2FB01LZ2WYKH&psig=AOvVaw0kMzU8ym0bdG513jaOhLy7&ust=1589696239055000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCi6o_et-kCFQAAAAAdAAAAABAD"}
     }, {
         timestamps:true,
 
