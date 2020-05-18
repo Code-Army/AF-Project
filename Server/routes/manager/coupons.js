b96249699d494f4ccbb1784ec5acae338652d54a
madushani <madushanidissanayake1998@gmail.com>
2020-05-19 00:39:42 +0530

    Update coupons.js

--- Server/routes/manager/coupons.js
+++ Server/routes/manager/coupons.js
@@ -36,37 +36,37 @@ router.route('/:id').delete((req,res) => {
         .then(() => res.json('coupon deleted.'))
         .catch(err => res.status(400).json('Error :' + err))
 });
-router.get("/pruduct_by_name", (req, res) => {
+router.get("/search/search_by_cname", (req, res) => {
     let type = req.query.type
-    let name = req.query.name
+    let cnames = req.query.cname
 
     if (type === "array") {
-        let names = req.query.name.split(',');
-        name = [];
-        name = names.map(item => {
+        let ids = req.query.cname.split(',');
+        cnames = [];
+        cnames = ids.map(item => {
             return item
         })
     }
 
-    console.log(name)
     //we need to find the product information that belong to product Id
-    Product.find({ 'productname': { $in: name } })
+    Coupon.find({ 'couponname': { $in: cnames } })
         .populate('writer')
-        .exec((err, product) => {
+        .exec((err, coupon) => {
+
             if(err) return req.status(400).send(err)
-            return res.status(200).send(product)
+            return res.status(200).send(coupon)
         })
 });
 router.route('/update/:id').post((req,res) => {
     Coupon.findById(req.params.id)
-        .then(producut => {
-            Coupon.couponname = req.body.couponname;
-            Coupon.couponcode = req.body.couponcode;
-            Coupon.couponamount = req.body.couponamount;
+        .then(coupon => {
+            coupon.couponname = req.body.couponname;
+            coupon.couponcode = req.body.couponcode;
+            coupon.couponamount = req.body.couponamount;
 
 
-            producut.save()
-                .then(() => res.json('Product Updated.'))
+            coupon.save()
+                .then(() => res.json('Coupon Updated.'))
                 .catch(err => res.status(400).json('Error :' +err));
         })
         .catch(err => res.status(400).json('Error :' + err))
