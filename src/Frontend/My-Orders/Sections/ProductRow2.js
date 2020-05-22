0390c98f2da0e76f4f0d8fff1ddbed76390abb06
pasan pramuditha <pasanpramuditha97@gmail.com>
2020-05-22 17:57:34 +0530

    Update ProductRow2.js

--- src/Frontend/My-Orders/Sections/ProductRow2.js
+++ src/Frontend/My-Orders/Sections/ProductRow2.js
@@ -9,11 +9,14 @@ export default class ProductRow extends Component{
 
     constructor(props) {
         super(props);
-
+        this.deleteOrder = this.deleteOrder.bind(this)
         this.state = {
             show:false,
+            delModel:false,
             rating: 1,
-            feedbackTxt:""
+            feedbackTxt:"",
+            delId:''
+
         }
     }
 
@@ -31,7 +34,8 @@ export default class ProductRow extends Component{
 
     handleclose = (e)=>{
         this.setState({
-            show:false
+            show:false,
+            delModel:false
         })
     }
 
@@ -45,6 +49,22 @@ export default class ProductRow extends Component{
             feedbackTxt:e.target.value
         });
 
+    }
+
+    updateStatus=(e)=>{
+
+        this.setState({
+            show:true
+        })
+        console.log(this.props.item._id)
+        const myOrder = {
+           status:"complete"
+
+        }
+
+        axios.post('http://localhost:5000/orders/update/'+this.props.item._id, myOrder)
+            .then(res => console.log(res.data));
+        console.log("order added sucessfully")
     }
 
     addFeedback(){
@@ -69,6 +89,25 @@ export default class ProductRow extends Component{
         })
     }
 
+    ConfirmDelete(){
+        axios.delete('http://localhost:5000/orders/'+this.state.delId)
+            .then(response => { console.log(response.data)});
+
+        this.setState({
+
+            delModel:false
+        })
+    }
+
+
+
+    deleteOrder(id){
+        this.setState({
+            delModel:true,
+            delId:id
+        })
+    }
+
     render() {
 
         return (
@@ -84,24 +123,39 @@ export default class ProductRow extends Component{
 
                                 <div className="product_number">
 
-
+                                    <span className="badge badge-danger" onClick={() => { this.deleteOrder(this.props.item._id) }}> <i className="fas fa-trash"></i></span>
                                 </div>
                             </div>
                             <div>
                                 <div className="product_image">
                                     <img
-                                        src={img1}
+                                        src={this.props.item.img}
 
                                     />
                                 </div>
                             </div>
                             <div className="product_name_container">
-                                <div className="product_name"><a href="product.html">{this.props.item.productId}</a></div>
-                                <div className="product_text">Second line for additional info</div>
+                                <div className="product_name"><a href="product.html">{this.props.item.productName}</a></div>
+                                <div className="product_text">Purchase Date - {this.props.item.createdAt}</div>
                             </div>
                         </div>
 
-                        <div className="product_price product_text"><button type="button" class="btn btn-success" onClick={this.handleshow.bind(this)}>Conform</button></div>
+                        {/*<div className="product_price product_text">*/}
+                        {/*    <button type="button" className="btn btn-success"*/}
+                        {/*            onClick={this.handleshow.bind(this)}>Conform*/}
+                        {/*    </button>*/}
+                        {/*</div>*/}
+                        {this.props.item.status === "pending"? <div className="product_price product_text">
+                            <button type="button" className="btn btn-success"
+                                   onClick={this.updateStatus.bind(this)} >Conform
+                            </button>
+                        </div> : <div className="product_price product_text">
+                            <button onClick={this.handleshow.bind(this)} type="button" className="btn btn-success"
+                            >feedback
+                            </button>
+                        </div> }
+
+
                         <div className="product_color product_text"></div>
                         <div className="product_size product_text"></div>
 
@@ -141,6 +195,21 @@ export default class ProductRow extends Component{
                     </Modal.Footer>
                 </Modal>
 
+
+                <Modal show={this.state.delModel} onHide={this.handleclose.bind(this)} animation={true}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>MY ORDER DELETE</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Do You Want Delete This Item ?</Modal.Body>
+                    <Modal.Footer>
+                        <button  class="btn btn-primary" onClick={this.handleclose.bind(this)}>
+                            Cancel
+                        </button>
+                        <button class="btn btn-danger" onClick={this.ConfirmDelete.bind(this)} >
+                            Delete
+                        </button>
+                    </Modal.Footer>
+                </Modal>
 
             </div>
 
